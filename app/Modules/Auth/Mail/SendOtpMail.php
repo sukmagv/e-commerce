d4f6ec4ebc2fb87c7733dcf175728617e6c5623e
Sukma Gladys <sukmagladysv@gmail.com>
2026-01-06 13:39:58 +0800

    fix: change message

--- app/Modules/Auth/Mail/SendOtpMail.php
+++ app/Modules/Auth/Mail/SendOtpMail.php
@@ -37,7 +37,7 @@ class SendOtpMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Send Otp Mail',
+            subject: 'OTP Code',
         );
     }
 
