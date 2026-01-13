37a400bade9f3586672e725f89c7f0f845f51414
Sukma Gladys <sukmagladysv@gmail.com>
2026-01-13 09:06:20 +0800

    feat: install telescope and add daily log

--- config/logging.php
+++ config/logging.php
@@ -52,9 +52,14 @@ return [
 
     'channels' => [
 
+        // 'stack' => [
+        //     'driver' => 'stack',
+        //     'channels' => explode(',', (string) env('LOG_STACK', 'single')),
+        //     'ignore_exceptions' => false,
+        // ],
         'stack' => [
-            'driver' => 'stack',
-            'channels' => explode(',', (string) env('LOG_STACK', 'single')),
+            'driver'            => 'stack',
+            'channels'          => ['daily'],
             'ignore_exceptions' => false,
         ],
 
@@ -65,12 +70,18 @@ return [
             'replace_placeholders' => true,
         ],
 
+        // 'daily' => [
+        //     'driver' => 'daily',
+        //     'path' => storage_path('logs/laravel.log'),
+        //     'level' => env('LOG_LEVEL', 'debug'),
+        //     'days' => env('LOG_DAILY_DAYS', 14),
+        //     'replace_placeholders' => true,
+        // ],
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
-            'days' => env('LOG_DAILY_DAYS', 14),
-            'replace_placeholders' => true,
+            'path'   => storage_path('logs/laravel.log'),
+            'level'  => env('LOG_LEVEL', 'debug'),
+            'days'   => 14,
         ],
 
         'slack' => [
