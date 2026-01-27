10d8aa76e3990338c356992c2e53b9f30c547f60
Sukma Gladys <sukmagladysv@gmail.com>
2026-01-27 09:49:03 +0800

    fix: revise update profile

--- app/Modules/Auth/Actions/UpdateProfileAction.php
+++ app/Modules/Auth/Actions/UpdateProfileAction.php
@@ -2,24 +2,22 @@
 
 namespace App\Modules\Auth\Actions;
 
-use App\Services\FileService;
-use App\Modules\Auth\Models\User;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
 use App\Modules\Auth\DTOs\UpdateProfileDTO;
+use App\Modules\Auth\Models\User;
+use App\Services\FileService;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class UpdateProfileAction
 {
-    public function __construct(protected FileService $fileService)
-    {}
+    public function __construct(protected FileService $fileService) {}
 
     /**
      * Update one or more customer profile
      *
-     * @param \App\Modules\Auth\Models\User $user
-     * @param \App\Http\Requests\Api\Customer\V1\UpdateProfileRequest $request
-     * @return \App\Modules\Auth\Models\User
+     * @param  \App\Modules\Auth\Models\User  $user
+     * @param  \App\Http\Requests\Api\Customer\V1\UpdateProfileRequest  $request
      */
     public function execute(UpdateProfileDTO $dto): User
     {
@@ -28,17 +26,21 @@ class UpdateProfileAction
             /** @var \App\Modules\Auth\Models\User $user */
             $user = Auth::user();
 
-            $user->update(['name' => $dto->name]);
+            if ($dto->name !== null) {
+                $user->update(['name' => $dto->name]);
+            }
 
-            $customerData = $dto->phone;
+            if ($dto->phone !== null) {
+                $customerData['phone'] = $dto->phone;
+            }
 
-            $oldPath = $user->customer->photo;
+            $oldPath = $user->customer?->photo;
 
             if ($dto->photo && $dto->photo instanceof UploadedFile) {
                 $customerData['photo'] = $this->fileService->updateOrCreate($dto->photo, $oldPath, 'profile');
             }
 
-            if ($user->customer && $customerData) {
+            if ($user->customer && ! empty($customerData)) {
                 $user->customer->update($customerData);
             }
 
